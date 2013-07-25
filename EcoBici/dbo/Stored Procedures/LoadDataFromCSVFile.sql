c674f2db6292cf8bde5e3d9abe66a44b8f88b4ce
Martin Mihura <mbmihura@gmail.com>
2013-07-25 18:29:13 -0300

    Creates table to store trips data, the proc to load it and post-deploy script to trriger the proc after a publication.

---
+++ EcoBici/dbo/Stored Procedures/LoadDataFromCSVFile.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[LoadDataFromCSVFile]
+AS
+	BULK INSERT [dbo].[Trips]
+	FROM 'c:\recorrido-bicis-2012.csv'
+	WITH
+	(
+		FIELDTERMINATOR = ',',
+		ROWTERMINATOR = '\n'
+	)
+RETURN 0
---
+++ EcoBici/dbo/Tables/Trips.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Trips]
+(
+	[IdUsuario] numeric(10, 0) NULL,
+	[EstacionOrigen] numeric(4, 0) NULL,
+	[FechaInicio] datetime NULL,
+	[EstacionDestino] numeric(4, 0) NULL,
+	[FechaFin] datetime NULL,
+	[TiempoUso] numeric(10, 0) NULL
+)
